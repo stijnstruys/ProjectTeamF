1d79d4f5dd04f6dcfad9aa65ea090d0df7643e74
jorne raeymaekers <jorne.raeymaekers@hotmail.com>
2013-02-07 14:56:42 +0100

    het werkt

--- src/main/java/be/kdg/teamf/controller/IndexController.java
+++ src/main/java/be/kdg/teamf/controller/IndexController.java
@@ -1,15 +1,18 @@
 package be.kdg.teamf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+@Controller
+@RequestMapping("/index")
+public class IndexController  {
 
-public class IndexController extends AbstractController {
-
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView handleRequestInternal(HttpServletRequest request,
                                                  HttpServletResponse response) throws Exception {
 
         ModelAndView model = new ModelAndView("index");
--- src/main/java/be/kdg/teamf/controller/UserController.java
+++ src/main/java/be/kdg/teamf/controller/UserController.java
@@ -8,8 +8,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,19 +21,26 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping("/user")
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView userPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("user");
+        return model;
+    }
+	/*@RequestMapping("/")
 	public String listContacts(Map<String, Object> map) {
 
 		map.put("user", new User());
 		map.put("userList", userService.listUsers());
 
 		return "user";
-	}
+	}  */
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String addContact(@ModelAttribute("user")
