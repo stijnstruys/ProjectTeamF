a7e7e19d1d9935d54423a1d280670fe99c82ee56
JeroenVerbunt <jeroen.verbunt@student.kdg.be>
2013-02-08 10:23:48 +0100

    test

---
+++ src/test/java/be/kdg/teamf/service/test.java
@@ -0,0 +1,11 @@
+package be.kdg.teamf.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jeroen
+ * Date: 8/02/13
+ * Time: 10:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
