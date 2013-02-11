2c38858fef4c88e045b335bc74e7c782b9807a1f
BART.LEEMANS <bart.leemans@student.kdg.be>
2013-02-11 14:55:10 +0100

    .

--- src/test/java/be/kdg/teamf/dao/TestUserDAO.java
+++ src/test/java/be/kdg/teamf/dao/TestUserDAO.java
@@ -10,7 +10,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created with IntelliJ IDEA.
@@ -37,4 +41,56 @@ public class TestUserDAO extends AbstractTransactionalJUnit4SpringContextTests {
         assertEquals("Expected firstname: ", "Bart Leemans", userDAO.findUser(u.getUserID()).getUsername());
     }
 
+    @Test
+    public void updateUser() {
+        User u = new User();
+        u.setEmail("bart@hotmail.com");
+        u.setLastName("Leemans");
+        u.setFirstName("Bart");
+        u.setUsername("Bart Leemans");
+        u.setTelephone("00306985587996");
+        userDAO.addUser(u);
+        u.setUsername("updated");
+        userDAO.updateUser(u);
+
+        assertEquals("De user is niet gewijzigd", "updated", userDAO.findUser("updated").getUsername());
+    }
+
+    @Test
+    public void listUsers() {
+        User user1 = new User();
+        user1.setUsername("Bart Leemans");
+        User user2 = new User();
+        user2.setUsername("Jeroen Dierckx");
+        userDAO.addUser(user1);
+        userDAO.addUser(user2);
+
+        assertEquals("Niet alle gebruikers worden opgehaald", 2, userDAO.listUsers().size());
+    }
+
+    @Test
+    public void deleteUser() {
+        User u = new User();
+        u.setUsername("Bart Leemans");
+        userDAO.addUser(u);
+        userDAO.deleteUser(u);
+        assertTrue("User not found", !userDAO.listUsers().contains(u));
+    }
+
+    @Test
+    public void findUser() {
+        User u = new User();
+        u.setUsername("Bart Leemans");
+        userDAO.addUser(u);
+        assertSame("you are not getting the expected user from the db", "Bart Leemans", userDAO.findUser(u.getUsername()).getUsername());
+    }
+
+    @Test
+    public void findUserId() {
+        User u = new User();
+        u.setUsername("Bart Leemans");
+        userDAO.addUser(u);
+        int id = u.getUserID();
+        assertSame("you are not getting the expected user from the db","Bart Leemans",userDAO.findUser(id).getUsername());
+    }
 }
--- src/test/java/be/kdg/teamf/service/TestUserService.java
+++ src/test/java/be/kdg/teamf/service/TestUserService.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
+import java.util.List;
+
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,5 +37,58 @@ public class TestUserService extends AbstractTransactionalJUnit4SpringContextTes
         userService.addUser(u);
 
         assertEquals("Expected firstname: ", "Bart Leemans", userService.findUser(u.getUserID()).getUsername());
+    }
+
+    @Test
+    public void listUsers() {
+        User user1 = new User();
+        user1.setUsername("Bart Leemans");
+        User user2 = new User();
+        user2.setUsername("Jeroen Dierckx");
+        userService.addUser(user1);
+        userService.addUser(user2);
+
+        assertEquals("Niet alle gebruikers worden opgehaald", 2, userService.listUsers().size());
+    }
+
+    @Test
+    public void deleteUser() {
+        User u = new User();
+        u.setUsername("Bart Leemans");
+        userService.addUser(u);
+        userService.deleteUser(u);
+        assertTrue("User not found", !userService.listUsers().contains(u));
+    }
+
+    @Test
+    public void findUser() {
+        User u = new User();
+        u.setUsername("Bart Leemans");
+        userService.addUser(u);
+        assertSame("you are not getting the expected user from the db", "Bart Leemans", userService.findUser(u.getUsername()).getUsername());
+    }
+
+    @Test
+    public void findUserId() {
+        User u = new User();
+        u.setUsername("Bart Leemans");
+        userService.addUser(u);
+        int id = u.getUserID();
+        assertSame("you are not getting the expected user from the db", "Bart Leemans", userService.findUser(id).getUsername());
+    }
+
+    @Test
+    public void updateUser() {
+        User u = new User();
+        u.setEmail("bart@hotmail.com");
+        u.setLastName("Leemans");
+        u.setFirstName("Bart");
+        u.setUsername("Bart Leemans");
+        u.setTelephone("00306985587996");
+        userService.addUser(u);
+        u.setUsername("updated");
+        userService.updateUser(u);
+
+        assertEquals("De user is niet gewijzigd", "updated", userService.findUser("updated").getUsername());
     }
 }
