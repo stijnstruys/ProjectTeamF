919eba0b6b5a7581a3abb28e49cc5f83bb2ddf22
Stijn Struys <stijn.struys@student.kdg.be>
2013-02-15 15:14:28 +0100

    TestStopPlaatsDAO

---
+++ src/test/java/be/kdg/teamf/dao/TestStopPlaatsDAO.java
@@ -0,0 +1,17 @@
+package be.kdg.teamf.dao;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stijn
+ * Date: 15-2-13
+ * Time: 15:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestStopPlaatsDAO {
+    @Test
+    public void addStopPlaats() {
+
+    }
+}
