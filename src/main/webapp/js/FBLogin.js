663f6aa5f4b6f6737d466edc0ff2be190b779960
Stijn Struys <stijn.struys@student.kdg.be>
2013-02-21 13:58:33 +0100

    notificiation + search

---
+++ src/main/webapp/js/FBLogin.js
@@ -0,0 +1,48 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stijn
+ * Date: 21-2-13
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
+ */
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : 'YOUR_APP_ID', // App ID
+        channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true  // parse XFBML
+    });
+
+    // Additional init code here
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            // connected
+        } else if (response.status === 'not_authorized') {
+            // not_authorized
+            login();
+        } else {
+            // not_logged_in
+            login();
+        }
+    });
+};
+
+function login() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            // connected
+        } else {
+            // cancelled
+        }
+    });
+}
+
+// Load the SDK Asynchronously
+(function(d){
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
