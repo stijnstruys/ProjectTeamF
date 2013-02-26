0dc61440fcfca918770ca5b71aee8b53f757d6d5
jorne raeymaekers <jorne.raeymaekers@hotmail.com>
2013-02-26 14:10:48 +0100

    brol

--- src/main/java/be/kdg/teamf/controller/IndexController.java
+++ src/main/java/be/kdg/teamf/controller/IndexController.java
@@ -2,7 +2,6 @@ package be.kdg.teamf.controller;
 
 import be.kdg.teamf.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,22 +11,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@RequestMapping("general/index.html")
+
 public class IndexController {
 
 
     @Autowired
     private TripService tripService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView handleRequestInternal(HttpServletRequest request,
-                                              HttpServletResponse response) throws Exception {
+    @RequestMapping(value ="general/index.html", method = RequestMethod.GET)
+    public ModelAndView indexPage(HttpServletRequest request,                                               HttpServletResponse response) throws Exception {
 
         ModelAndView model = new ModelAndView("General/index");
-        //be.kdg.teamf.model.addObject("msg", "hello world");
+
 
         return model;
     }
+    @RequestMapping(value ="general/login.html", method = RequestMethod.GET)
+    public ModelAndView loginPage(HttpServletRequest request,                                               HttpServletResponse response) throws Exception {
 
+        ModelAndView model = new ModelAndView("General/login");
+
+
+        return model;
+    }
 
 }
