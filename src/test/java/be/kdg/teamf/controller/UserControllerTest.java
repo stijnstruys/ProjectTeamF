1dd528f005fa242c7b80529bd82d7f7320a55f22
Stijn Struys <stijn.struys@student.kdg.be>
2013-03-04 14:46:15 +0100

    test

---
+++ src/test/java/be/kdg/teamf/controller/UserControllerTest.java
@@ -0,0 +1,11 @@
+package be.kdg.teamf.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stijn
+ * Date: 4-3-13
+ * Time: 14:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserControllerTest {
+}
