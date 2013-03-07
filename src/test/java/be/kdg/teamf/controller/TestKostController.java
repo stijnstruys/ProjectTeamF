ebf9727bd66b974ebe86a1a8063c85585edd1a2e
Stijn Struys <stijn.struys@student.kdg.be>
2013-03-07 16:02:36 +0100

    Test kosten

---
+++ src/test/java/be/kdg/teamf/controller/TestKostController.java
@@ -0,0 +1,11 @@
+package be.kdg.teamf.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stijn
+ * Date: 7-3-13
+ * Time: 15:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestKostController {
+}
