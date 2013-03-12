8e299db271de8f55f6489ff25f6a02faaceb4ce3
Jeroen-Dierckx <jeroen.dierckx.1@gmail.com>
2013-03-12 20:23:25 +0100

    tripsUser

--- src/main/java/be/kdg/teamf/controller/TripController.java
+++ src/main/java/be/kdg/teamf/controller/TripController.java
@@ -249,7 +249,7 @@ public class TripController {
     @RequestMapping(value = "/service/getOpenTrips", method = RequestMethod.GET)
     public
     @ResponseBody
-    Trip getOpenTrips(){
+    List<Trip> getOpenTrips(){
         List<Trip> trips = new ArrayList<Trip>(tripService.listTrips());
         List<Trip> openTrips = new ArrayList<Trip>();
         for(Trip t:trips){
@@ -260,11 +260,22 @@ public class TripController {
                 openTrips.add(temp);
             }
         }
-        return openTrips.get(0);
+        return openTrips;
     }
     @RequestMapping(value = "/service/tripList", method = RequestMethod.POST, headers = "Accept=application/json")
     public List<Trip> getTripList(@ModelAttribute("user") User user, BindingResult result,HttpServletRequest request) {
     //public Trip getTripList(@ModelAttribute("user") User user, BindingResult result,HttpServletRequest request) {
         return tripService.listTrips();
+    }
+
+    @RequestMapping(value = "/service/getTripsUser", method = RequestMethod.POST,headers = "Accept=application/json")
+
+    public Trip[] login(@ModelAttribute("user") User user,@RequestParam(value = "userid",required = true,defaultValue = "0") String userid, BindingResult result,HttpServletRequest request) {
+
+        List<Trip> trips = new ArrayList<Trip>(tripService.listUserTrips(Integer.parseInt(userid)));
+        Trip[] tripsArray = new Trip[trips.size()];
+        trips.toArray(tripsArray);
+
+        return tripsArray;
     }
 }
